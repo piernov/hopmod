f780cad71f9bd6b5400d2a75ed03574429f5b143
t.poechtrager@gmail.com <t.poechtrager@gmail.com@d7818858-e1b4-11dd-a738-5fe2e3cfbcc5>
2013-05-10 16:44:55 +0000

    build iOS bins with openssl support

--- pre-built/trunk/iOS/compile_ios.sh
+++ pre-built/trunk/iOS/compile_ios.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export REVISION=`svn info --xml | grep -m 1 -E 'revision *\= *"[0-9]+" *>' | grep -oE '[0-9]+'`
 rm -rf .ios_build
-mkdir .ios_build && cd .ios_build && cmake .. -DIOS=1 -DWITH_OPENSSL=No -DWITH_LUASQL=No && make -j8 && make install && rm -rf .ios_build && exit 0
+mkdir .ios_build && cd .ios_build && cmake .. -DIOS=1 -DWITH_LUASQL=No && make -j8 && make install && rm -rf .ios_build && exit 0
 exit 1
